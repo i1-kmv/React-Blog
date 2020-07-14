9ae370c9f838217f13efddb66dbc3a30f467e773
i1-kmv <i1-kmv@mail.ru>
2020-07-14 18:24:43 +0200

    Подготовил структуру приложения, сделал разбивку на компоненты, создал компоненты app, header, search

---
+++ src/components/app/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppHeader from '../app-header';
+import SearchPanel from "../search-panel";
+
+const App = () => {
+    return (  
+        <div className="app">
+              <AppHeader/>
+              <div className="search-panel d-flex">
+                  <SearchPanel/>
+              </div>
+        </div>
+    )
+}
+
+export default App;
