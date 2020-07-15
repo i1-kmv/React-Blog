e9047531a60fa55710ef3cc03e3b9e07457a85c6
i1-kmv <i1-kmv@mail.ru>
2020-07-15 11:29:57 +0200

    Создал и стилезовал все компоненты проекта

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from "../search-panel";
+import PostStatusFilter from "../post-status-filter";
+import PostList from "../post-list";
+import PostAddForm from "../post-add-form";
+import "./app.css"
+
 
 const App = () => {
     return (  
@@ -8,7 +13,10 @@ const App = () => {
               <AppHeader/>
               <div className="search-panel d-flex">
                   <SearchPanel/>
+                  <PostStatusFilter/>
               </div>
+              <PostList/>
+              <PostAddForm/>
         </div>
     )
 }
