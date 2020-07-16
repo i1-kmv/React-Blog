d036c19a45b39f2f2d933d0dde6f4e6ae1632c17
i1-kmv <i1-kmv@mail.ru>
2020-07-16 11:48:42 +0200

    Перенес все данные в глобальный стэйт, очистил компоненту post-list-item, реализовал методы для выделения поста и лайка, также настроил счетчик лайков и постов

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,22 +16,23 @@ export default class  App extends Component {
         super(props);
         this.state = {
             data: [
-              {label: "Going to learn React", important: true, id: 1},
-              {label: "That is so good", important: false, id: 2},
-              {label: "I need beer", important: false, id: 3}
+              {label: "Going to learn React", important: true, like: false, id: 1},
+              {label: "That is so good", important: false, like: false, id: 2},
+              {label: "I need beer", important: false, like: false, id: 3}
             ]
         };
         this.deleteItem = this.deleteItem.bind(this);
         this.addItem = this.addItem.bind(this);
+        this.onToggleImportant = this.onToggleImportant.bind(this);
+        this.onToggleLiked = this.onToggleLiked.bind(this);
         this.maxId = 4;
     }
 
     deleteItem(id) {
         this.setState(({data}) => {
             const index = data.findIndex(elem => elem.id === id);
-            const before = data.slice(0, index);
-            const after = data.slice(index + 1);
-            const newArr = [...before, ...after];
+            
+            const newArr = [...data.slice(0, index), ...data.slice(index + 1)];
             return {
                 data: newArr
             }
@@ -53,25 +54,59 @@ export default class  App extends Component {
         })
     }
 
+    onToggleImportant(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+
+            const old = data[index];
+            const newItem = {...old, important: !old.important};
+
+            const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+            return {
+                data: newArr
+            }
+        })
+    }
+
+    onToggleLiked(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+
+            const old = data[index];
+            const newItem = {...old, like: !old.like};
+
+            const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+            return {
+                data: newArr
+            }
+        })
+    }
+
     render() {
+
+        const liked = this.state.data.filter(item => item.like).length;
+        const allPosts = this.state.data.length;
+
         return (  
           <div className={s.app}>
-                <AppHeader/>
+                <AppHeader
+                  liked = {liked}
+                  allPosts = {allPosts}
+                />
                 <div className="search-panel d-flex">
                     <SearchPanel/>
                     <PostStatusFilter/>
                 </div>
                 <PostList 
                 posts={this.state.data}
-                onDelete={this.deleteItem}/>
+                onDelete={this.deleteItem}
+                onToggleImportant={this.onToggleImportant}
+                onToggleLiked={this.onToggleLiked}/>
                 <PostAddForm
                 onAdd={this.addItem}
                 />
           </div>
         )
-    }
-
-   
-    
+    } 
 }
 
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,13 +3,13 @@ import PostListItem from "../post-list-item";
 import { ListGroup, ListGroupItem } from 'reactstrap';
 import "./post-list.css"
 
-const PostList = ({posts, onDelete}) => {
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
 
     const elements = posts.map((elem) => {
         const {id, ...itemProps} = elem;
         return (
             <li key = {id} className="list-group-item">
-                <PostListItem {...itemProps} onDelete={() => onDelete(id)}/>
+                <PostListItem {...itemProps} onDelete={() => onDelete(id)} onToggleImportant={() => onToggleImportant(id)} onToggleLiked={() => onToggleLiked(id)}/>
             </li>
         ) 
     })
