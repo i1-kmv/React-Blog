cf717a2908895ee76efbda3f9c99ae8aa9801598
i1-kmv <i1-kmv@mail.ru>
2020-07-19 15:46:52 +0200

    Updates

---
+++ precache-manifest.b974974c60015b4c193b9809ca870a30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cdacb48114594b37d8c7361416bf26",
+    "url": "/i1-kmv/React-Blog/index.html"
+  },
+  {
+    "revision": "4f9b0975a7aaae346cf3",
+    "url": "/i1-kmv/React-Blog/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "dd08c75d74eafb621188",
+    "url": "/i1-kmv/React-Blog/static/css/main.171d9cf1.chunk.css"
+  },
+  {
+    "revision": "4f9b0975a7aaae346cf3",
+    "url": "/i1-kmv/React-Blog/static/js/2.7ad61ae4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/i1-kmv/React-Blog/static/js/2.7ad61ae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd08c75d74eafb621188",
+    "url": "/i1-kmv/React-Blog/static/js/main.0fb92255.chunk.js"
+  },
+  {
+    "revision": "55eb0648bc0cbe4a7deb",
+    "url": "/i1-kmv/React-Blog/static/js/runtime-main.d10df2eb.js"
+  }
+]);
